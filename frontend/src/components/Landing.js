b650ebabdaffc29eb449261e0a2507d7dbaa7adb
Brendan-Fisher <btfisher1229@gmail.com>
2021-03-23 15:16:05 -0400

    Added background and content box for landing page

---
+++ frontend/src/components/Landing.js
@@ -0,0 +1,16 @@
+import  React, { Component }from "react";
+import './styles/Landing.css'; 
+
+class Landing extends Component {
+    render() {
+        return (
+            <div id="container" className="container">
+                <div id="content" className="row">
+                    
+                </div>
+            </div>
+        )
+    }
+}
+
+export default Landing;
